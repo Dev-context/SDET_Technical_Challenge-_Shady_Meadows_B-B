3c8f69e3563629f89371e1072ddc425a5aaf0176
Vinicius Oliveira Frasson <vinicius.smtp.cmd@gmail.com>
2026-03-22 17:33:40 +0000

    feature:add function to get random room

--- Karate_API_Test/shadyMeadowsBB/src/test/java/helpers/DataGenerator.java
+++ Karate_API_Test/shadyMeadowsBB/src/test/java/helpers/DataGenerator.java
@@ -1,12 +1,16 @@
 package helpers;
 
-import java.util.List;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.github.javafaker.Faker;
 
 public class DataGenerator {
 
     private static final Faker faker = new Faker();
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public static String getRandomFirstName() {
         return faker.name().firstName();
@@ -20,18 +24,23 @@ public class DataGenerator {
         return faker.bool().bool();
     }
 
-    public static String getRandonChekinDate(String checkin) {
-
-        return "";
-
+    public static String getRandomCheckinDate() {
+        return LocalDate.now()
+                .plusDays(faker.number().numberBetween(1, 10))
+                .format(formatter);
     }
 
-    public static String getRandonCheckoutDate(String checkoutDate) {
-        return "";
-    }
+    public static Map<String, String> getRandomPairOfDates() {
 
-    public static int getRandomRoomId(List<Integer> list) {
-        return list.get(faker.random().nextInt(list.size()));
+        LocalDate start = LocalDate.now().plusDays(new Faker().number().numberBetween(1, 60));
+        LocalDate end = start.plusDays(2);
+        Map<String, String> dates = new HashMap<>();
+
+        dates.put("checkin", start.format(DateTimeFormatter.ISO_LOCAL_DATE));
+        dates.put("checkout", end.format(DateTimeFormatter.ISO_LOCAL_DATE));
+
+        return dates;
+
     }
 
 }
