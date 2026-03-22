049d5d35af10471f27e9c68903336ffb24a0590c
Dev-Context <vinicius.smtp.cmd@gmail.com>
2026-03-22 10:23:17 +0000

    fix:pipeline files

--- UI_Test/playwright.config.ts
+++ UI_Test/playwright.config.ts
@@ -22,20 +22,20 @@ export default defineConfig({
   },
 
   projects: [
-    // {
-    //   name: "chromium",
-    //   use: { ...devices["Desktop Chrome"] },
-    // },
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
 
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     {
       name: 'Mobile Chrome',
